cd88297780cee4443be0f2ae96b3a0e291a9e48d
Tilman Raendchen <TRaendchen@outlook.com>
2019-12-28 17:17:58 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const Client = require("./lib/classes/Client.js");
+let client = new Client();
+
+client.login(process.env.BOT_TOKEN)
+    .catch(console.error);
+
+module.exports = client;
