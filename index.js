c21db64804c44b5ea273808c5e9a6b12b45d6ce3
Tilman Raendchen <traendchen@outlook.com>
2020-09-20 18:24:12 +0100

    Convert spaces to tabs for everything

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const Client = require("./lib/classes/Client.js");
 let client = new Client();
 
 client.login(process.env.BOT_TOKEN)
-    .catch(console.error);
+	.catch(console.error);
 
 module.exports = client;
